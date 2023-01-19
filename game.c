d7adb8049528213db34970bbaade2b3b279141d5
LoonaFox <sudoloonafox@gmail.com>
2023-01-20 04:21:55 +1300

    Implemented buffer swap

--- game.c
+++ game.c
@@ -8,7 +8,7 @@
 const int screenWidth = 1920;
 const int screenHeight = 1080;
 
-float framerate = 0.5
+float framerate = 0.5;
 
 void delay(int milliseconds){
     long pause;
@@ -19,27 +19,33 @@ void delay(int milliseconds){
     while( (now-then) < pause )
         now = clock();
 }
+void randomBufferFill(int* buffer, int width, int height);
+void swapActiveScreenBuffer(int** buffer_1, int** buffer_2);
 
 int main(){
+	int* screenBuffer = malloc(sizeof(int)*screenWidth*screenHeight);
+	int* drawBuffer = malloc(sizeof(int)*screenWidth*screenHeight);
 
-	uint32_t screenBuffer[screenWidth * screenHeight];
-	SDL_Window * window = SDL_CreateWindow("Conway's Game of Life", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, 0);
-	SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	SDL_Texture * screen = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, screenWidth, screenHeight);
-	int size = screenWidth*screenHeight;
-	player.angle = 0.0f;
-	clock_t lastUpdate = clock();
+	SDL_Window* window = SDL_CreateWindow("Conway's Game of Life", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, 0);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	SDL_Texture* screen = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, screenWidth, screenHeight);
+	// clock_t lastUpdate = clock();
+
+	srand(time(NULL));
+	
+	randomBufferFill(drawBuffer, screenWidth, screenHeight);
+
 	// Game loop
 	int running = 1; // adding kill functionality	
 	while(running){
 		SDL_Event event; // queues event
 		clock_t startGameLoop = clock();
-
+		/*
 		// delta time calculations
 		clock_t currentUpdate = clock();
 		float deltaTime =(double)(currentUpdate - lastUpdate)/CLOCKS_PER_SEC;
 		lastUpdate = currentUpdate;
-		
+		*/
 		while(SDL_PollEvent(&event)){ // dequeues event and checks its value
 			switch(event.type){
 			case SDL_QUIT:
@@ -56,6 +62,8 @@ int main(){
 	SDL_RenderCopy(renderer, screen, NULL, NULL);
 	SDL_RenderPresent(renderer);
 
+	swapActiveScreenBuffer(&screenBuffer, &drawBuffer);
+
 	// frame cap
 	clock_t endGameLoop = clock();
 	double elapsedLoopTime = (double)(endGameLoop - startGameLoop) / CLOCKS_PER_SEC;
@@ -66,5 +74,20 @@ int main(){
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
+	free(screenBuffer);
+	free(drawBuffer);
+
 	return 0;
+}
+
+void randomBufferFill(int* buffer, int width, int height){
+	for(int i=0; i<width*height; i++){
+		buffer[i] = rand() % 2;
+	}
+}
+
+void swapActiveScreenBuffer(int** buffer_1, int** buffer_2){
+	int* temp = *buffer_1;
+	*buffer_1 = *buffer_2;
+	*buffer_2 = temp;
 }
